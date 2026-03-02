5498336f4b0b14802078634a2cb54a00840441d3
João Pedro Sousa e Silva <joaopedev@outlook.com>
2026-03-02 15:40:30 -0300

    Applying toaster as answers;

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { Toaster } from "react-hot-toast";
 import { PrivateRoute } from "./PrivateRoute";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -8,12 +8,12 @@ import Marketplace from "../pages/Marketplace";
 export function AppRoutes() {
   return (
     <BrowserRouter>
+
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Navigate to="/marketplace" replace />} />
-
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
         <Route
           path="/marketplace"
           element={
