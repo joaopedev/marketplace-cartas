1373f8a2c63df64533d3d0af363263133446a0ce
João Pedro Sousa e Silva <joaopedev@outlook.com>
2026-03-02 15:25:38 -0300

    Apply function login;

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { useLogin } from "../hooks/useLogin";
 import {
   Container,
   LeftSide,
@@ -11,28 +12,42 @@ import {
   RegisterText,
   StyledLink,
 } from "./Login.styles";
+import Logo from "../assets/image.png";
 
 export default function Login() {
+  const { login } = useLogin();
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    login({
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    });
+  }
+
   return (
     <Container>
       <LeftSide>
         <LogoWrapper>
-          <h1>Marketplace</h1>
+          <img src={Logo} alt="Logo INMETA" />
         </LogoWrapper>
       </LeftSide>
 
       <RightSide>
         <FormWrapper>
           <Title>Login</Title>
-
-          <Form>
-            <Input type="email" placeholder="Email" />
-            <Input type="password" placeholder="Senha" />
+          <Form onSubmit={handleSubmit}>
+            <Input type="email" name="email" placeholder="Email" />
+            <Input type="password" name="password" placeholder="Senha" />
             <Button type="submit">Entrar</Button>
           </Form>
-
           <RegisterText>
-            Não possui conta? <StyledLink to="/register">Cadastre-se</StyledLink>
+            Não possui conta?{" "}
+            <StyledLink to="/register">Cadastre-se</StyledLink>
           </RegisterText>
         </FormWrapper>
       </RightSide>
