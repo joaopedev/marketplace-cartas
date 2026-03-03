abf54d21c3dd61f01fd4b5aa0adf3bfc1c4cf16c
João Pedro Sousa e Silva <joaopedev@outlook.com>
2026-03-03 17:50:46 -0300

    Fix type ReactNode;

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { useAuthStore } from "../store/auth.store";
 
 interface PrivateRouteProps {
